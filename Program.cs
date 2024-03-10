1c78e05c724a30f15d53cd6d0e5863def9af3367
Eleaz <Eleaz@THEGOATPC>
2024-03-10 14:28:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Reflection;
+
+namespace bookStoreRecord
+{
+    internal class Program
+    {
+        public record Bookstore(int ID, string Title, string Author, double Price);
+        static void Main(string[] args)
+        {
+            Bookstore book1 = new(10, "American Rust", "Philipp Meyer", 15.00);
+            Console.WriteLine(book1);
+            Bookstore book2 = new(12, "The Secret Garden", "Frances Hodgson Burnett", 11.95);
+            Console.WriteLine(book2);
+            Bookstore book3 = new(14, "Signature Classics", "C.S. Lewis", 39.99);
+            Console.WriteLine(book3);
+        }
+    }
+}
